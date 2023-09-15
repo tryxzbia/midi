72f8739f913a48f99cffeef021072f5801df49c3
tryxzbia <108531051+tryxzbia@users.noreply.github.com>
2023-09-15 19:27:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//Declarar e chamar a função, executar o som através do play
+/*function tocaSomPom () {
+  document.querySelector('#som_tecla_pom').play();
+}
+//querySelector para selecionar elementos
+//concede o acesso ao código, atribuir para tocar o som
+document.querySelector('.tecla_pom').onclick = tocaSomPom;*/
