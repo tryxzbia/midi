ea59507cfab288a5117c2196eec0be40ff8ae1a9
tryxzbia <108531051+tryxzbia@users.noreply.github.com>
2023-11-18 13:09:29 -0300

    s

--- main.js
+++ main.js
@@ -1,84 +1,52 @@
-/*O método query-Selector busca apenas o primeiro elemento correspondente ao seletor. Exemplo abaixo*/
-//tecla Pom
-/*function tocaSomPom () {
-  document.querySelector('#som_tecla_pom').play();
-}
+function tocaSom (seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+  if (elemento && elemento.localName === 'audio') {
+      elemento.play();
+  }
+  else {
+      //alert('Elemento não encontrado');
+      console.log('Elemento não encontrado ou seletor inválido');
+  }
 
-//tecla clap
-function tocaSomClap () {
-  document.querySelector('#som_tecla_clap').play();
-}
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-//tecla tim
-function tocaSomTim () {
-  document.querySelector('#som_tecla_tim').play();
-}
-
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
-
-//tecla puff
-function tocaSomPuff () {
-  document.querySelector('#som_tecla_puff').play();
-}
-
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-
-//tecla splash
-function tocaSomSplash () {
-  document.querySelector('#som_tecla_splash').play();
-}
-
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;
-
-//tecla toim
-function tocaSomToim () {
-  document.querySelector('#som_tecla_toim').play();
-}
-
-document.querySelector('.tecla_toim').onclick = tocaSomToim;
-
-//tecla psh
-function tocaSomPsh () {
-  document.querySelector('#som_tecla_psh').play();
-}
-
-document.querySelector('.tecla_psh').onclick = tocaSomPsh;
-
-//tecla tic
-function tocaSomTic () {
-  document.querySelector('#som_tecla_tic').play();
-}
-
-document.querySelector('.tecla_tic').onclick = tocaSomTic;
-
-//tecla tom
-function tocaSomTom () {
-  document.querySelector('#som_tecla_tom').play();
-}
-
-document.querySelector('.tecla_tom').onclick = tocaSomTom;
-*/
-
-//const listaDeTeclas = document.querySelectorAll('.tecla');
-
-//listaDeTeclas[0].onclick = tocaSomPom;
-
-/*function tocaSomPom () {
-  document.querySelector('#som_tecla_pom').play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0; //loop infinito então
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while (contador < listaDeTeclas.length) {
-  listaDeTeclas[contador].onclick = tocaSomPom;
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`; //template string
 
-  contador = contador + 1; //então conta
+  tecla.onclick = function () {
+      tocaSom(idAudio);
+  }
 
-  console.log(contador);//imprimir o contador várias vezes
-}*/
+//eventos do teclado onkeydowne onkeyup.
+//adicionar e remover classes em um elemento HTML através do JavaScript, com as funções add e remove do classList.   
+  tecla.onkeydown = function (evento) {
+
+      if (evento.code === 'Space' || evento.code === 'Enter') {
+          tecla.classList.add('ativa');
+      }
+
+  }
+
+  tecla.onkeyup = function () {
+      tecla.classList.remove('ativa');
+  }
+
+}
+
+/*Template string é uma forma de facilitar a exibição de texto no JavaScript, 
+ele permite agrupar textos (strings) com outros tipos de informação, como number, boolean, array, entre outros.
+
+Estrutura de repetição for(significa para). 
+Estrutura condicional if(significa se).
+
+Operador de igualdade ==, estritamente igual (===), e o operador or (||).
+
+A estruturas condicionais if e else juntas. 
+O operador not equals (!=), operador lógico and (&&) e o valor null.*/
